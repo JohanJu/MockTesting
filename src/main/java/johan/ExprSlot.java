2bf80ca8affe0d654e81ee8d2874492df587285e
JohanJu <johanj93@gmail.com>
2016-02-23 16:44:46 +0100

    add sheeteasy that tests get method

---
+++ src/main/java/johan/ExprSlot.java
@@ -0,0 +1,31 @@
+package johan;
+
+import java.io.IOException;
+
+public class ExprSlot implements Slot {
+
+	public ExprSlot(String value, Sheet sheet) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void set(String string) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getLabelText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Double value() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
